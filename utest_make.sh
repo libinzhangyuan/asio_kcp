d1d029c43b52d85d8ab9e226823b7fdfeaa86412
zhangyuan <libinzhagyuan@gmail.com>
2015-07-09 15:26:45 +0800

    zhangyuan master utest_make.sh

--- utest_make.sh
+++ utest_make.sh
@@ -5,9 +5,7 @@ export BOOST_LIB_PATH
 BOOST_INC_PATH=/usr/local/include
 export BOOST_INC_PATH
 
-`rm client_with_asio/client_with_asio 2>/dev/null ;\
-    rm server/server 2>/dev/null ;\
-    rm server_lib/asio_kcp_server.a 2>/dev/null;\
+`rm server_lib/asio_kcp_server.a 2>/dev/null;\
     rm asio_kcp_utest/asio_kcp_utest 2>/dev/null;\
 `
 
@@ -15,13 +13,10 @@ echo "" && echo "" && echo "[-------------------------------]" && echo "   essen
     cd ./essential/ && make && \
 echo "" && echo "" && echo "[-------------------------------]" && echo "   asio_kcp" && echo "[-------------------------------]" && \
     cd ../server_lib/ && make && \
-echo "" && echo "" && echo "[-------------------------------]" && echo "   server" && echo "[-------------------------------]" && \
-    cd ../server/ && make && \
-echo "" && echo "" && echo "[-------------------------------]" && echo "   client_with_asio" && echo "[-------------------------------]" && \
-    cd ../client_with_asio/ && make && \
 echo "" && echo "" && echo "[-------------------------------]" && echo "   asio_kcp_utest" && echo "[-------------------------------]" && \
     cd ../asio_kcp_utest/ && make && \
-echo ""
+echo "" && \
+    ./asio_kcp_utest
 
 # restore old path.
 cd $OLD_PWD
