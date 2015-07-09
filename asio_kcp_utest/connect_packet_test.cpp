ae09d44317e5067b065d9e236bebecd1a7f192a9
zhangyuan <libinzhagyuan@gmail.com>
2015-07-09 10:56:29 +0800

    zhangyuan master test the connect_packet_test, fix bug

--- asio_kcp_utest/connect_packet_test.cpp
+++ asio_kcp_utest/connect_packet_test.cpp
@@ -1,6 +1,15 @@
 #include "gtest/gtest.h"
+#include "../util/connect_packet.hpp"
 
+using namespace asio_kcp;
 
-TEST(FactorialTest, Negative) {
-    EXPECT_EQ(1, 1);
+TEST(ConnectPacketTest, Normal) {
+    std::string packet = making_connect_packet();
+    EXPECT_TRUE(is_connect_packet(packet.c_str(), packet.size()));
+}
+
+TEST(ConnectSendBackConvTest, Normal) {
+    std::string packet = making_send_back_conv_packet(232);
+    ASSERT_TRUE(is_send_back_conv_packet(packet.c_str(), packet.size())) << "packet: " << packet;
+    ASSERT_TRUE(grab_conv_from_send_back_conv_packet(packet.c_str(), packet.size()) == 232);
 }
--- util/connect_packet.cpp
+++ util/connect_packet.cpp
@@ -16,25 +16,25 @@ std::string making_connect_packet(void)
 bool is_connect_packet(const char* data, size_t len)
 {
     return (len == sizeof(ASIO_KCP_CONNECT_PACKET) &&
-        memcmp(data, ASIO_KCP_CONNECT_PACKET, sizeof(ASIO_KCP_CONNECT_PACKET)) == 0);
+        memcmp(data, ASIO_KCP_CONNECT_PACKET, sizeof(ASIO_KCP_CONNECT_PACKET) - 1) == 0);
 }
 
 bool is_send_back_conv_packet(const char* data, size_t len)
 {
     return (len > sizeof(ASIO_KCP_SEND_BACK_CONV_PACKET) &&
-        memcmp(data, ASIO_KCP_SEND_BACK_CONV_PACKET, sizeof(ASIO_KCP_SEND_BACK_CONV_PACKET)) == 0);
+        memcmp(data, ASIO_KCP_SEND_BACK_CONV_PACKET, sizeof(ASIO_KCP_SEND_BACK_CONV_PACKET) - 1) == 0);
 }
 
 std::string making_send_back_conv_packet(uint32_t conv)
 {
-    char str_send_back_conv[1024] = "";
-    size_t n = snprintf(str_send_back_conv, sizeof(str_send_back_conv), "%s%u", ASIO_KCP_SEND_BACK_CONV_PACKET, conv);
+    char str_send_back_conv[256] = "";
+    size_t n = snprintf(str_send_back_conv, sizeof(str_send_back_conv), "%s %u", ASIO_KCP_SEND_BACK_CONV_PACKET, conv);
     return std::string(str_send_back_conv, n);
 }
 
 uint32_t grab_conv_from_send_back_conv_packet(const char* data, size_t len)
 {
-    uint32_t conv = std::atol(data + sizeof(ASIO_KCP_SEND_BACK_CONV_PACKET) - 1);
+    uint32_t conv = std::atol(data + sizeof(ASIO_KCP_SEND_BACK_CONV_PACKET));
     return conv;
 }
 
