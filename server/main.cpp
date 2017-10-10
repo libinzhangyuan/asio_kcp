3a3b995f3dd3d7422026434af9d63577fe92f07d
zhangyuan <libinzhangyuan@gmail.com>
2017-10-10 11:49:47 +0800

    zhangyuan code clean

--- server/main.cpp
+++ server/main.cpp
@@ -27,50 +27,50 @@ void dummyOutput(const char* msg, int len)
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    // Check command line arguments.
-    if (argc != 3)
+    try
     {
-      std::cerr << "Usage: server <address> <port>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    server 0.0.0.0 80\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    server 0::0 80\n";
-      return 1;
+        // Check command line arguments.
+        if (argc != 3)
+        {
+            std::cerr << "Usage: server <address> <port>\n";
+            std::cerr << "  For IPv4, try:\n";
+            std::cerr << "    server 0.0.0.0 80\n";
+            std::cerr << "  For IPv6, try:\n";
+            std::cerr << "    server 0::0 80\n";
+            return 1;
+        }
+
+        //system("mkdir /var/log/asio_kcp");
+
+        // g2log
+        //
+        std::string path_to_log_file("/var/log/asio_kcp_log/");
+        g2LogWorker logger(argv[0], path_to_log_file);
+        g2::initializeLogging(&logger);
+        AK_LOG(INFO) << "Start";
+
+        // muduo log
+        //
+        std::string basename = "/var/log/asio_kcp_log/ak_muduo_log";
+        size_t rollSize = 500*1000*1000;
+        bool threadSafe = true;
+        int flushInterval = 3; // seconds
+        int checkEveryN = 1024;
+        g_logFile.reset(new ::muduo::LogFile(basename, rollSize, threadSafe, flushInterval, checkEveryN));
+        muduo::Logger::setOutput(dummyOutput);
+        LOG_INFO << "start";
+
+
+        // Initialise the server.
+        server s(argv[1], argv[2]);
+
+        // Run the server until stopped.
+        s.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "exception: " << e.what() << "\n";
     }
 
-    //system("mkdir /var/log/asio_kcp");
-
-    // g2log
-    //
-    std::string path_to_log_file("/var/log/asio_kcp_log/");
-    g2LogWorker logger(argv[0], path_to_log_file);
-    g2::initializeLogging(&logger);
-    AK_LOG(INFO) << "Start";
-
-    // muduo log
-    //
-    std::string basename = "/var/log/asio_kcp_log/ak_muduo_log";
-    size_t rollSize = 500*1000*1000;
-    bool threadSafe = true;
-    int flushInterval = 3; // seconds
-    int checkEveryN = 1024;
-    g_logFile.reset(new ::muduo::LogFile(basename, rollSize, threadSafe, flushInterval, checkEveryN));
-    muduo::Logger::setOutput(dummyOutput);
-    LOG_INFO << "start";
-
-
-    // Initialise the server.
-    server s(argv[1], argv[2]);
-
-    // Run the server until stopped.
-    s.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    return 0;
 }
--- server/server.hpp
+++ server/server.hpp
@@ -7,27 +7,27 @@
 #include "../server_lib/server.hpp"
 
 class server
-  : private boost::noncopyable
+: private boost::noncopyable
 {
-public:
-  /// Construct the server to listen on the specified TCP address and port
-  explicit server(const std::string& address, const std::string& port);
+    public:
+        /// Construct the server to listen on the specified TCP address and port
+        explicit server(const std::string& address, const std::string& port);
 
-  /// Run the server's io_service loop.  Must set_callback first then call run. Do not change callback after run.
-  void run();
+        /// Run the server's io_service loop.  Must set_callback first then call run. Do not change callback after run.
+        void run();
 
-private:
-  /// Handle a request to stop the server.
-  void handle_stop();
+    private:
+        /// Handle a request to stop the server.
+        void handle_stop();
 
-  /// The io_service used to perform asynchronous operations.
-  boost::asio::io_service io_service_;
+        /// The io_service used to perform asynchronous operations.
+        boost::asio::io_service io_service_;
 
-  /// The signal_set is used to register for process termination notifications.
-  boost::asio::signal_set signals_;
+        /// The signal_set is used to register for process termination notifications.
+        boost::asio::signal_set signals_;
 
-  /// The connection manager which owns all live connections.
-  kcp_svr::server kcp_server_;
+        /// The connection manager which owns all live connections.
+        kcp_svr::server kcp_server_;
 };
 
 #endif // _SERVER_HPP
